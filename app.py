e4a0064ccc33509c8f1b38fdb6ec9372381737f3
Vamshi <vkgaming906@gmail.com>
2024-04-27 22:29:08 +0530

    new commit

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 import streamlit as st
+import wikipedia as wp
+
+
+def getWikiSearchSuggestions(query):
+    try:
+        page = wp.search(query,results = 5)
+        suggestions = [str(p.title) for p in page]
+        return suggestions
+    except:
+        return ["Nothing Found"]
 
 def main():
     st.title("WikiQuest")
+    st.header("Your WikiPedia Guide") # Header
 
+    st.markdown("***") # HR
+
+    st.text("")
+
+    st.write("Search For Anything")
+    text_search = st.text_input("") # Search Bar
+
+    # Getting Search Suggestions : 
+    suggestions = getWikiSearchSuggestions(text_search)
+
+    if suggestions:
+        selected_suggestion = st.selectbox("Select",options = suggestions)
+
+    st.header(text_search)
+    
 
 if __name__ == "__main__":
     main()
