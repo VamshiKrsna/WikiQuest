fcf58116edc87ea9ae311a33ee504454f426cf4e
Vamshi <vkgaming906@gmail.com>
2024-04-28 09:36:23 +0530

    new Commit

--- app.py
+++ app.py
@@ -1,36 +1,24 @@
 import streamlit as st
 import wikipedia as wp
-
+from streamlit_searchbox import st_searchbox
 
 def getWikiSearchSuggestions(query):
-    try:
-        page = wp.search(query,results = 5)
-        suggestions = [p for p in page]
-        return suggestions
-    except:
-        return ["Nothing Found"]
+    return wp.search(query) if query else ["Nothing Found"]
 
 def main():
     st.title("WikiQuest")
     st.header("Your WikiPedia Guide") # Header
-
     st.markdown("***") # HR
-
     st.text("")
+    # Search bar and suggestions : 
+    selected_val = st_searchbox(
+        getWikiSearchSuggestions,
+        key="wiki_searchbox",
+        placeholder="Search From Wikipedia"
+    )
+    if selected_val:
+        st.write("Selected Value :",selected_val)
 
-    st.write("Search For Anything")
-    text_search = st.text_input("") # Search Bar
-
-    # Getting Search Suggestions : 
-    suggestions = getWikiSearchSuggestions(text_search)
-
-    if suggestions:
-        st.write("Suggestions:")
-        for suggestion in suggestions:
-            st.write(suggestion)
-    else:
-        st.write("No suggestions available.")
     
-
 if __name__ == "__main__":
     main()
